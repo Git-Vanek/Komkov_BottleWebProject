d1314cf49c516aa19e9cf837b1fa6b0dc30be66f
Ivan <ki16082005@gmail.com>
2024-04-03 12:38:08 +0300

    Добавление функции обработки формы, которая данные, вводимые пользователем, записывались в словарь

--- Komkov_BottleWebProject/myform.py
+++ Komkov_BottleWebProject/myform.py
@@ -1,11 +1,20 @@
+import pdb
 from datetime import date
 from bottle import post, request
 
 @post('/home', method='post')
 def my_form():
+    # создание словаря
+    questions = {}
     # получение адреса из формы
     mail = request.forms.get('ADRESS')
     # получение имени из формы
     username = request.forms.get('USERNAME')
+    # получение вопроса из формы
+    question = request.forms.get('QUEST')
+    # создание словаря
+    questions = {mail : question}
+    # запуск отладчика
+    pdb.set_trace()
     # возвращение сообщения
     return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (username, mail, date.today())
